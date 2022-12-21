4f1704136294504d649ce014004e77889e1fdc35
hamsterTears <katen-91@mail.ru>
2022-12-21 13:57:58 +0300

    modified brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
-console.log("Welcome to the Brain Games!");
-import greeting from '../src/cli.js'; 
-greeting ()
+// eslint-disable-next-line import/extensions
+import greeting from '../src/cli.js';
+
+// eslint-disable-next-line no-console
+console.log('Welcome to the Brain Games!');
+greeting();
