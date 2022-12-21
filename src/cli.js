9fd955be996973dc66f4b273c49cbac5d10adccf
hamsterTears <katen-91@mail.ru>
2022-12-21 14:00:32 +0300

    modified

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,11 @@
+/* eslint-disable no-redeclare */
+/* eslint-disable no-console */
 import readlineSync from 'readline-sync';
 
- const greeting = (userName)=> {
-    var userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
- }
-
+const greeting = (userName) => {
+  // eslint-disable-next-line no-var
+  var userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
 
 export default greeting;
-
-
