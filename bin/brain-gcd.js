4510fe4243afdb06f6f71f7aebe6a75f3bab93e6
Великая Катерина <KaterinaTheGreat@MacBook-Pro-Dara.local>
2023-01-03 01:20:59 +0300

    create gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import gcd from '../src/games/brain-gcd-game.js';
+
+// eslint-disable-next-line no-unused-expressions
+gcd;
---
+++ src/games/brain-gcd-game.js
@@ -0,0 +1,19 @@
+import logicGame from '../index.js';
+
+const getGcd = (x, y) => (y === 0 ? x : getGcd(y, x % y));
+
+const getAnswer = () => {
+  const randomCount1 = Math.floor(Math.random() * 10) + 1;
+  const randomCount2 = Math.floor(Math.random() * 10) + 1;
+  const question = `${randomCount1} ${randomCount2}`;
+  console.log(question);
+  const expectedAnswer = getGcd(randomCount1, randomCount2);
+  return expectedAnswer;
+};
+
+const gcd = () => {
+  const description = 'Find the greatest common divisor of given numbers.';
+  logicGame(description, getAnswer);
+};
+gcd();
+export default gcd;
