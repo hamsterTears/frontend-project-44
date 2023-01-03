163dd5fa84fb03f0a7a6a3cbf30c1f79918a8f2b
Великая Катерина <KaterinaTheGreat@MacBook-Pro-Dara.local>
2023-01-03 20:18:53 +0300

    fix import

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import progression from '../src/games/brain-gcd-game.js';
+import progression from '../src/games/brain-progression-game.js';
 
 // eslint-disable-next-line no-unused-expressions
 progression;
