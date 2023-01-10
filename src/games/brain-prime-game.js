62d981647d3de6a491a9597f2f62172637eb7955
Великая Катерина <KaterinaTheGreat@MacBook-Pro-Dara.local>
2023-01-10 22:27:36 +0300

    fix

--- src/games/brain-prime-game.js
+++ src/games/brain-prime-game.js
@@ -3,14 +3,13 @@ import logicGame from '../index.js';
 const getTask = () => {
   const randomCount = Math.floor(Math.random() * 100) + 1;
   const task = (`Question: ${randomCount}`);
-  if (((randomCount > 1) && (randomCount % randomCount === 0))
-  && ((randomCount % 2 !== 0) || (randomCount === 2))) {
-    const expectedAnswer = 'yes';
-    return [task, expectedAnswer];
-  } if ((randomCount % 2 === 0) && (randomCount !== 2)) {
+  if ((randomCount % 2 === 0) && (randomCount !== 2)) {
     const expectedAnswer = 'no';
     return [task, expectedAnswer];
-  } return task;
+  }
+
+  const expectedAnswer = 'yes';
+  return [task, expectedAnswer];
 };
 
 const isPrime = () => {
