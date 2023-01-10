7d2773f5dd9485e537e093ccb87c415aef75b5a7
Великая Катерина <KaterinaTheGreat@MacBook-Pro-Dara.local>
2023-01-10 21:38:39 +0300

    modified index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import readlineSync from 'readline-sync';
 import greeting from './cli.js';
 
 const logicGame = (description, getTask) => {
+  const userName = greeting();
   console.log(description);
   let correctAnswers = 0;
-  const name = greeting;
   while (correctAnswers < 3) {
     const [task, expectedAnswer] = getTask();
     console.log(task);
@@ -14,9 +14,9 @@ const logicGame = (description, getTask) => {
       correctAnswers += 1;
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`);
-      console.log(`"Let's try again, ${name}!"`);
+      console.log(`"Let's try again, ${userName}!"`);
       return correctAnswers === 3;
     }
-  } return console.log(`'Congratulations, ${name}!'`);
+  } return console.log(`'Congratulations, ${userName}!'`);
 };
 export default logicGame;
