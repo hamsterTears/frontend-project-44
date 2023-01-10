88ac6c508b2bea52060eba5893132b68004f47df
Великая Катерина <KaterinaTheGreat@MacBook-Pro-Dara.local>
2023-01-10 21:39:29 +0300

    modified even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 import evenGame from '../src/games/brain-even-game.js';
 
-// eslint-disable-next-line no-unused-expressions
-evenGame;
+evenGame();
--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -19,4 +19,3 @@ const evenGame = () => {
 };
 
 export default evenGame;
-evenGame();
