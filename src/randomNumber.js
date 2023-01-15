0a143348b0c1c286da4e33d3f498684a2965d85a
hamsterTears <katen-91@mail.ru>
2023-01-15 22:54:36 +0300

    modified randomNumber

--- src/randomNumber.js
+++ src/randomNumber.js
@@ -1,5 +1,5 @@
-const getRandomNumber = () => {
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
+const getRandomNumber = (min = 0, max = 100) => {
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
   return randomNumber;
 };
 export default getRandomNumber;
